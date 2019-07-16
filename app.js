cacbb6402d5a2f0376a179291eb806e8d6b88abe
Erik Lotz <elo@jsmithy.de>
2019-07-16 16:54:51 +0200

    Refactoring. Extract function.

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/ping', function(request, response) {
+function pingResourceGet(request, response) {
     console.log("'/ping' requested");
     response.send('Hi, Duke!');
-});
+}
+app.get('/ping', pingResourceGet);
 
 app.listen(port, () => console.log(`Express REST app listening on port ${port}!`));
