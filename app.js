c57530ab172166894b6b53e97b28b3e40987605e
Erik Lotz <elo@jsmithy.de>
2019-07-16 17:14:32 +0200

    Add a uuid to the JSON response.

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const uuid = require('uuid/v1');
 const app = express();
 const port = 3000;
 
 function pingResourceGet(request, response) {
     reply = {
+        "uuid": uuid(),
         "reply": "Hi, Duke!"
     };
     
     console.log("'/ping' requested");
     response.send(reply);
 }
+
 app.get('/ping', pingResourceGet);
 
 app.listen(port, () => console.log(`Express REST app listening on port ${port}!`));
